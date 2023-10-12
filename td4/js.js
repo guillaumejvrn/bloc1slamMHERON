d957cd52c9df9e08a051efb16ba83b00a1becd0e
guillaumejvrn <guillaume.jenvrin@sts-sio-caen.info>
2023-10-12 14:47:52 +0200

    js

---
+++ td4/js.js
@@ -0,0 +1,4 @@
+const bt = document.body.querySelector("button");
+bt.addEventListener("click",()=>alert("hellowwwwwwwwwwwwwwwwww"));
+//alert("COUSCOUS");
+//document.body.innerHTML = "<h1>couscous</h1>";
