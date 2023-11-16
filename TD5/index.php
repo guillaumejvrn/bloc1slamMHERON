aa322337e7b238bee8cdc208bf365875fa2bdd24
guillaumejvrn <guillaume.jenvrin@sts-sio-caen.info>
2023-11-16 16:54:06 +0100

    aze

--- TD5/index.php
+++ TD5/index.php
@@ -23,17 +23,47 @@
 
         <form method="get" action="index.php">
             <label for="cb">combien de lignes</label>
-            <input name="cb" type="text" required>
+            <input name="cb" type="number">
         </form>
 
         <?php
         $_tab = array ('Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione similique nemo odit nulla reiciendis, consequatur recusandae eos enim magnam id cum amet velit quia dicta pariatur sint, quaerat repellat est?');
         if (isset($_GET['cb'])) {
-            for ($nb = 1; $nb == $_GET['cb']; $nb++){
-                echo $_tab;
+            for ($nb = 0; $nb < $_GET['cb']; $nb++){
+                echo $_tab[$nb];
             }
         }
         ?>
+        <p>aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa</p>
+
+        <form method="get" action="index.php">
+            <label for="li">  combien de lignes  </label>
+            <input name="li" type="number" required><br>
+
+            <label for="co">combien de colones</label>
+            <input name="co" type="number" required><br>
+            <input type="submit" value="Soumettre">
+        </form>
+        <textarea name="cafard" id="jsp" cols="30" rows="10">
+        <?php
+        if (isset($_GET['li']) && isset($_GET['co'])) {
+            $li = $_GET['li'];
+            $co = $_GET['co'];
+            echo '<p>est ce que c bon pour vous</p>';
+
+            echo '<table>';
+            for ($i = 0; $i < $li; $i++) {
+                echo '<tr rowspan="1">';
+                for ($j = 0; $j < $co; $j++) {
+                    echo '<td colspan="1">Cellule ' . ($i + 1) . '-' . ($j + 1) . '</td>';
+                }
+                echo '</tr>';
+            }
+            echo '</table>';
+        }
+        ?>
+        </textarea>
+        
 
     </body>
 </html>
