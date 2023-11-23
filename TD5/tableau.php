b1953beaaa782d1b1b86218f9fe87e0f54a8afef
guillaumejvrn <guillaume.jenvrin@sts-sio-caen.info>
2023-11-23 16:58:58 +0100

    battlesage

--- TD5/tableau.php
+++ TD5/tableau.php
@@ -9,12 +9,12 @@
 
         <a href="index.php">index</a>
 
-        <form method="get" action="index.php">
+        <form method="get" action="tableau.php">
                     <label for="li">  combien de lignes  </label>
-                    <input name="li" type="number" required><br>
+                    <input name="li" type="number" ><br>
 
                     <label for="co">combien de colones</label>
-                    <input name="co" type="number" required><br>
+                    <input name="co" type="number" ><br>
                     <input type="submit" value="Soumettre">
                 </form>
                 <textarea name="cafard" id="jsp" cols="30" rows="10">
@@ -35,7 +35,41 @@
                     echo '</table>';
                 }
                 ?>
+
                 </textarea>
+                
+    <form method="get" action="tableau.php">
+        <label for="fusion">  combien de cases voulez vous fusioner  </label>
+        <input name="fusion" type="number"><br>
+        <input type="submit" value="soumettre">
+    </form>
+
+
+    <?php
+    
+    if(isset($_GET['fusion'])){
+        $lignefusionne = $_GET['fusion'];
+        
+        echo "<form>";
+
+        for($i = 0; $i < $lignefusionne; $i++){
+            echo '<label for="startcase"> de la case </label> <input name="startcase" type="number">'.'<label for="endcase"> a la case </label> <input name="endcase" type="number"><br>';
+        }
+        echo '<input type="submit" value="soumettre">';
+        echo "</form>";
+    }
+    
+    ?>
+
+    <?php
+    for($i = 0; $i < $lignefusionne; $i++){
+        if(isset($_GET[''])){
+            
+        }
+    }
+    
+    ?>
+                
                 <?php
                 if (isset($_GET['li']) && isset($_GET['co'])) {
                     $li = $_GET['li'];
